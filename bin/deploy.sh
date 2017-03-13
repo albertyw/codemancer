e208e1935edcd7eacca44948cf92703d9cb28a2d
Albert Wang <albertyw@mit.edu>
2017-03-12 23:31:16 -0700

    Remove uwsgi from server config files

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,17 +5,9 @@ cd /var/www/website/ || exit 1
 git checkout master
 git pull
 
-# Update python packages
-virtualenvlocation=$(which virtualenvwrapper.sh)
-# shellcheck source=/dev/null
-source "$virtualenvlocation"
-workon GIT_REPOSITORY_NAME
-pip install -r requirements.txt
-
 # Make generated static file directory writable
 sudo chown www-data app/static/gen
 sudo chown www-data app/static/.webassets-cache
 
 # Restart services
 sudo service nginx restart
-sudo service uwsgi restart
--- bin/setup.sh
+++ bin/setup.sh
@@ -2,9 +2,7 @@
 
 # Setup directories
 sudo mkdir -p /var/www/log/nginx
-sudo mkdir -p /var/www/log/uwsgi
 sudo chmod -R 777 /var/www/log
-sudo ln -s /var/www/log/uwsgi /var/log/uwsgi
 sudo ln -s /var/www/log/nginx /var/log/nginx
 sudo ln -s ~/website /var/www/website
 
@@ -31,22 +29,3 @@ sudo rm -r /var/www/html
 openssl dhparam -out /etc/nginx/ssl/dhparams.pem 2048
 # Copy server.crt and server.key to /etc/nginx/ssl
 sudo service nginx restart
-
-# Install uwsgi
-sudo apt-get install -y uwsgi uwsgi-plugin-python3 python3-dev python3-setuptools
-
-# Install python/pip/virtualenvwrapper
-curl https://bootstrap.pypa.io/get-pip.py | sudo python3
-sudo pip3 install virtualenvwrapper
-
-# Install python packages
-# shellcheck disable=SC1091
-. /usr/local/bin/virtualenvwrapper.sh
-mkvirtualenv --python=/usr/bin/python3 GIT_REPOSITORY_NAME
-pip3 install -r /var/www/website/requirements.txt
-sudo ln -s "$HOME/.virtualenvs" /var/www/.virtualenvs
-
-# Set up uwsgi
-sudo ln -s /var/www/website/config/uwsgi/website.ini /etc/uwsgi/apps-available/website.ini
-sudo ln -s ../apps-available/website.ini /etc/uwsgi/apps-enabled/website.ini
-sudo service uwsgi restart
