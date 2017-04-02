e0f10c7afd8241812211dbce206edd1d10a00afd
Albert Wang <albertyw@mit.edu>
2017-04-02 00:36:20 -0700

    Simplify Weather.condition

--- currently/js/app.js
+++ currently/js/app.js
@@ -1,3 +1,46 @@
+var weatherIconConversions = {
+  "chanceflurries": "p",
+  "chancesnow": "p",
+  "/ig/images/weather/flurries.gif": "]",
+  "chancesleet": "4",
+  "chancerain": "7",
+  "chancetstorms": "x",
+  "tstorms": "z",
+  "nt_tstorms": "z",
+  "clear": "v",
+  "sunny": "v",
+  "cloudy": "`",
+  "flurries": "]",
+  "nt_flurries": "]",
+  "fog": "g",
+  "hazy": "g",
+  "nt_fog": "g",
+  "nt_hazy": "g",
+  "mostlycloudy": "1",
+  "partlysunny": "1",
+  "partlycloudy": "1",
+  "mostlysunny": "1",
+  "sleet": "3",
+  "nt_sleet": "3",
+  "rain": "6",
+  "nt_rain": "6",
+  "snow": "o",
+  "nt_snow": "o",
+  // Night Specific
+  "nt_chanceflurries": "a",
+  "nt_chancerain": "8",
+  "nt_chancesleet": "5",
+  "nt_chancesnow": "[",
+  "nt_chancetstorms": "c",
+  "nt_clear": "/",
+  "nt_sunny": "/",
+  "nt_cloudy": "2",
+  "nt_mostlycloudy":
+  "nt_partlysunny": "2",
+  "nt_partlycloudy": "2",
+  "nt_mostlysunny": "2"
+};
+
 var Loader = {
   loader: $('#loader'),
   show: function() {
@@ -166,98 +209,11 @@ var Weather = {
       // We can't find the code
       code = null;
     }
-    switch(code) {
-
-      case "chanceflurries":
-      case "chancesnow":
-        return "p";
-
-      case "/ig/images/weather/flurries.gif":
-        return "]";
-
-      case "chancesleet":
-        return "4";
-
-      case "chancerain":
-        return "7";
-
-      case "chancetstorms":
-        return "x";
-
-      case "tstorms":
-      case "nt_tstorms":
-        return "z";
-
-      case "clear":
-      case "sunny":
-        return "v";
-
-      case "cloudy":
-        return "`";
-
-      case "flurries":
-      case "nt_flurries":
-        return "]";
-
-      case "fog":
-      case "hazy":
-      case "nt_fog":
-      case "nt_hazy":
-        return "g";
-
-      case "mostlycloudy":
-      case "partlysunny":
-      case "partlycloudy":
-      case "mostlysunny":
-        return "1";
-
-      case "sleet":
-      case "nt_sleet":
-        return "3";
-
-      case "rain":
-      case "nt_rain":
-        return "6";
-
-      case "snow":
-      case "nt_snow":
-        return "o";
-
-      // Night Specific
-
-      case "nt_chanceflurries":
-        return "a";
-
-      case "nt_chancerain":
-        return "8";
-
-      case "nt_chancesleet":
-        return "5";
-
-      case "nt_chancesnow":
-        return "[";
-
-      case "nt_chancetstorms":
-        return "c";
-
-      case "nt_clear":
-      case "nt_sunny":
-        return "/";
-
-      case "nt_cloudy":
-        return "2";
-
-      case "nt_mostlycloudy":
-      case "nt_partlysunny":
-      case "nt_partlycloudy":
-      case "nt_mostlysunny":
-        return "2";
-
-
-      default:
-        console.log("MISSING", code);
+    var weatherIconCode = weatherIconConversions[code];
+    if (weatherIconCode === undefined) {
         return "T";
     }
+    return weatherIconCode;
   },
 
   render: function(wd) {
