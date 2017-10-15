ab63791b75572e70e9ce1879b06f82ba4a524689
Albert Wang <git@albertyw.com>
2017-10-15 10:40:06 -0700

    Update background refresh with demo mode

--- codemancer/js/demo.js
+++ codemancer/js/demo.js
@@ -1,19 +1,23 @@
 var originalDate = Date;
+var originalUpdateBackgroundColorPeriod = updateBackgroundColorPeriod;
 
 mockDate = function() {
-    var date = new Date();
+    var date = new originalDate();
     var timestep = date.getSeconds() + date.getMinutes() * 60;
     var hours = timestep % 24;
     date.setHours(hours);
-    var minutes = 0;
-    date.setMinutes(minutes);
+    date.setMinutes(0);
+    date.setSeconds(0);
     return date;
 }
 
 function startDemo() {
     Date = mockDate;
+    updateBackgroundColorPeriod = 500;
+    updateBackgroundColor();
 }
 
 function stopDemo() {
     Date = originalDate;
+    updateBackgroundColorPeriod = originalUpdateBackgroundColorPeriod;
 }
