2c42e79b17626141a4f12e6a7f2573ef490246ee
Albert Wang <git@albertyw.com>
2018-09-04 23:28:03 -0700

    Install node and get javascript dependencies in container

--- bin/container_setup.sh
+++ bin/container_setup.sh
@@ -6,5 +6,11 @@ cd $DIR/..
 # Set locale
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && locale-gen
 
+# Install nginx dependencies
+
+# Install node dependencies
+apt install -y nodejs npm
+npm install
+
 # Write current version to temp file
 git rev-parse HEAD > codemancer/version
