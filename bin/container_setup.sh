243005f85aabda11d39ba14d7cfe3bd37303fdc9
Albert Wang <git@albertyw.com>
2018-09-04 23:52:08 -0700

    Set up in-container nginx

--- bin/container_setup.sh
+++ bin/container_setup.sh
@@ -6,7 +6,17 @@ cd $DIR/..
 # Set locale
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && locale-gen
 
-# Install nginx dependencies
+# Install nginx
+add-apt-repository ppa:nginx/stable
+apt-get update
+apt-get install -y nginx
+
+# Configure nginx
+rm -rf /etc/nginx/sites-available
+rm -rf /etc/nginx/sites-enabled/*
+ln -s ~/codemancer/config/sites-available-container/app /etc/nginx/sites-enabled/codemancer-app
+rm -r /var/www/html
+service nginx restart
 
 # Install node dependencies
 apt install -y nodejs npm
