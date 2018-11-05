aee73f8543a6608ab7e6a05b9d356942a7beea47
Albert Wang <git@albertyw.com>
2018-11-04 17:25:44 -0800

    Switch to debian docker container

--- bin/container_setup.sh
+++ bin/container_setup.sh
@@ -7,6 +7,7 @@ cd $DIR/..
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && locale-gen
 
 # Install node dependencies
+curl -sL https://deb.nodesource.com/setup_11.x | bash -
 apt-get update
 apt-get install -y nodejs npm
 npm install
