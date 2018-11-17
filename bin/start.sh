b903bc8a36ccdf1c7ee872e67f64467c25cef292
Albert Wang <git@albertyw.com>
2018-11-16 22:20:22 -0800

    Enable node-static server

--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+BASE_DIR="$DIR"/..
+cd $BASE_DIR
+
+node_modules/.bin/static --host-address=0.0.0.0 --port=5002 --gzip=true "$BASE_DIR/codemancer"
