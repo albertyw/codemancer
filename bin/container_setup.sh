970fdf27bae68745403e8534762a88552adb051f
Albert Wang <git@albertyw.com>
2018-12-31 15:01:25 +0800

    Install libcurl4-openssl-dev for nodegit

--- bin/container_setup.sh
+++ bin/container_setup.sh
@@ -9,7 +9,7 @@ sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && locale-g
 # Install node dependencies
 curl -sL https://deb.nodesource.com/setup_11.x | bash -
 apt-get update
-apt-get install -y nodejs npm
+apt-get install -y nodejs npm libcurl4-openssl-dev
 npm install
 
 # Compile code
