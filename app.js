08f5c81a6ca4786be4b3aa880052e585a0385446
Albert Wang <git@albertyw.com>
2019-01-21 19:58:53 -0800

    Integrate mustache into app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const child_process = require("child_process");
 const console = require("console");
 const express = require("express");
+const fs = require("fs");
 const morgan = require("morgan");
+const mustache = require("mustache");
 const path = require("path");
 const rfs = require("rotating-file-stream");
 
@@ -9,12 +11,27 @@ require("dotenv").config();
 const port = process.env.LISTEN_PORT;
 const app = express();
 
+// Set up logging
 app.use(morgan("combined"));
 var accessLogStream = rfs("access.log", {
     interval: "1d",
     path: path.join(__dirname, "logs", "app")
 });
 app.use(morgan("combined", {stream: accessLogStream }));
+
+// Set up mustache
+// To set functioning of mustachejs view engine
+app.engine("html", function (filePath, options, callback) {
+    fs.readFile(filePath, function (err, content) {
+        if(err) {
+            return callback(err);
+        }
+        var rendered = mustache.to_html(content.toString(),options);
+        return callback(null, rendered);
+    });
+});
+app.set("views", path.join(__dirname, "codemancer"));
+app.set("view engine","html");
 
 let version = "";
 function getAndRespondVersion(res) {
