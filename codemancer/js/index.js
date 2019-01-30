ed98e60b804ea6d190249d6a13d1e94a0907fd2b
Albert Wang <git@albertyw.com>
2019-01-29 23:56:40 -0800

    Move rollbar into bundle

--- codemancer/js/index.js
+++ codemancer/js/index.js
@@ -1,5 +1,16 @@
 window.AppDate = Date;
 
+const rollbar = require("rollbar-browser");
+var rollbarConfig = {
+    accessToken: window.ROLLBAR_ACCESS_TOKEN,
+    captureUncaught: true,
+    payload: {
+        environment: window.ENVIRONMENT,
+    }
+};
+const Rollbar = rollbar.init(rollbarConfig);
+window.Rollbar = Rollbar;
+
 require("./clock");
 require("./weather");
 require("./refresh");
