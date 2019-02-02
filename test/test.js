0abf11329f63c2f94b595a509b4ee35785fa95e1
Albert Wang <git@albertyw.com>
2019-02-02 00:41:39 -0800

    Set up mochify

--- test/test.js
+++ test/test.js
@@ -1,8 +1,1 @@
-const mocha = require("mocha");
-mocha.setup("bdd");
-
 require("./test_calendar");
-
-mocha.checkLeaks();
-mocha.globals([]);
-mocha.run();
--- test/test_calendar.js
+++ test/test_calendar.js
@@ -3,7 +3,7 @@ const expect = require("chai").expect;
 const calendar = require("../codemancer/js/calendar");
 
 describe("trimString", function() {
-    it("should be available", function() {
+    it("should return a trimmed string", function() {
         const trimmedString = calendar.trimString(" asdf  ");
         expect(trimmedString).to.be.equal("asdf");
     });
