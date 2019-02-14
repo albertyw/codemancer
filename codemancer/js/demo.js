6fc72137dd1534e89e0376a7019fe05fe8a81f7a
Albert Wang <git@albertyw.com>
2019-02-13 23:01:36 -0800

    Move mockDate to a utils

--- codemancer/js/demo.js
+++ codemancer/js/demo.js
@@ -1,36 +1,24 @@
 const background = require('./background');
+const util = require('./util');
 
 const originalUpdateBackgroundColorPeriod = background.updateBackgroundColorPeriod;
-let demoOn = false;
-
-const mockDate = function() {
-    const date = new Date();
-    const timestep = date.getSeconds() + date.getMinutes() * 60;
-    const hours = timestep % 24;
-    date.setHours(hours);
-    date.setMinutes(0);
-    date.setSeconds(0);
-    return date;
-};
 
 function startDemo() {
-    window.AppDate = mockDate;
     background.updateBackgroundColorPeriod = 500;
     background.updateBackgroundColor();
 }
 
 function stopDemo() {
-    window.AppDate = Date;
     background.updateBackgroundColorPeriod = originalUpdateBackgroundColorPeriod;
 }
 
 function toggleDemo() { // eslint-disable-line no-unused-vars
+    const demoOn = util.toggleDemo();
     if (demoOn) {
         stopDemo();
     } else {
         startDemo();
     }
-    demoOn = !demoOn;
 }
 
 module.exports = {
