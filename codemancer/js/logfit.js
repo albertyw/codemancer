03687e710a7175176d58f0c43f4176f39a1f8a1b
Albert Wang <git@albertyw.com>
2019-03-16 16:24:20 -0700

    Do not set up logfit if a token is not provided

--- codemancer/js/logfit.js
+++ codemancer/js/logfit.js
@@ -1,8 +1,10 @@
-const LogFit = require('logfit');
+if (process.env.LOGFIT_TOKEN) {
+    const LogFit = require('logfit');
 
-var LogFitConfig = {
-    source: process.env.LOGFIT_TOKEN,
-};
+    var LogFitConfig = {
+        source: process.env.LOGFIT_TOKEN,
+    };
 
-const logfit = new LogFit(LogFitConfig);
-logfit.report();
+    const logfit = new LogFit(LogFitConfig);
+    logfit.report();
+}
