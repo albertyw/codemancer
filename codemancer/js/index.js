494b746bca1a86883781fe685d40cc678d587636
Albert Wang <git@albertyw.com>
2019-03-16 16:22:47 -0700

    Do not set up rollbar if a token is not provided

--- codemancer/js/index.js
+++ codemancer/js/index.js
@@ -1,13 +1,15 @@
-const rollbar = require('rollbar-browser');
-var rollbarConfig = {
-    accessToken: process.env.ROLLBAR_CLIENT_ACCESS,
-    captureUncaught: true,
-    payload: {
-        environment: process.env.ENVIRONMENT,
-    }
-};
-const Rollbar = rollbar.init(rollbarConfig);
-window.Rollbar = Rollbar;
+if (process.env.ROLLBAR_CLIENT_ACCESS) {
+    const rollbar = require('rollbar-browser');
+    var rollbarConfig = {
+        accessToken: process.env.ROLLBAR_CLIENT_ACCESS,
+        captureUncaught: true,
+        payload: {
+            environment: process.env.ENVIRONMENT,
+        }
+    };
+    const Rollbar = rollbar.init(rollbarConfig);
+    window.Rollbar = Rollbar;
+}
 
 require('./google');
 require('./clock');
