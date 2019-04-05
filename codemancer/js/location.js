9070f9b076d47a03d93437a35611ddcf0997fa46
Albert Wang <git@albertyw.com>
2019-04-05 00:02:12 -0700

    Switch location.js to Promise

--- codemancer/js/location.js
+++ codemancer/js/location.js
@@ -1,11 +1,9 @@
-const $ = require('jquery');
-const Q = require('q');
-
 const targetLocation = {
   wfo: 'MTR', x: '88', y: '128',
   lat: 37.778519, lng: -122.40564,
 };
 const geocodingAPIKey = process.env.GEOCODING_API_KEY;
+const geocodingURL = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 function unique(array) {
   return Array.from(new Set(array));
@@ -15,22 +13,25 @@ const Location = {
   targetLocation: targetLocation,
 
   getDisplayName: function(location) {
-    return Q.when($.ajax({
-      url : 'https://maps.googleapis.com/maps/api/geocode/json',
-      data: {
-        latlng: location.lat +','+ location.lng,
-        sensor: false,
-        key: geocodingAPIKey,
-      },
-      dataType: 'json'
-    }))
-      .then(function(data) {
-        if (data.status === 'OK') {
-          return Location.parseDisplayName(data);
-        } else {
-          throw new Error('Failed to geocode');
-        }
-      });
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      let url = geocodingURL;
+      url += '?latlng=' + encodeURIComponent(location.lat + ',' + location.lng);
+      url += '&sensor=false';
+      url += '&key=' + encodeURIComponent(geocodingAPIKey);
+      xhr.open('GET', url);
+      xhr.onload = () => resolve(xhr.responseText);
+      xhr.onerror = () => reject(xhr.statusText);
+      xhr.send();
+    }).then((data) => {
+      data = JSON.parse(data);
+      if (data.status === 'OK') {
+        return Location.parseDisplayName(data);
+      }
+      throw new Error('Failed to geocode');
+    }, (error) => {
+      throw new Error('Failed to geocode' + error);
+    });
   },
 
   parseDisplayName: function(data) {
