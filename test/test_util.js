e5744372256fea221b9bf5a183946e271b06aa83
Albert Wang <git@albertyw.com>
2019-04-05 22:52:57 -0700

    Add test for toggleDemo

--- test/test_util.js
+++ test/test_util.js
@@ -1,3 +1,10 @@
 const expect = require('chai').expect; // eslint-disable-line no-unused-vars
 
-const calendar = require('../codemancer/js/util'); // eslint-disable-line no-unused-vars
+const util = require('../codemancer/js/util'); // eslint-disable-line no-unused-vars
+
+describe('toggleDemo', () => {
+  let demoStatus = util.toggleDemo();
+  expect(demoStatus).to.be.true;
+  demoStatus = util.toggleDemo();
+  expect(demoStatus).to.be.false;
+});
