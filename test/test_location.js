99ec1a54de01c759a51d4d0a319a691335012ce6
Albert Wang <git@albertyw.com>
2019-04-07 18:21:49 -0700

    Test location error conditions

--- test/test_location.js
+++ test/test_location.js
@@ -1,6 +1,8 @@
 const expect = require('chai').expect;
+const sinon = require('sinon');
 
 const Location = require('../codemancer/js/location');
+const Rollbar = require('../codemancer/js/rollbar');
 const locationData = require('./map_fixture.json');
 
 describe('Location.targetLocation', () => {
@@ -14,10 +16,52 @@ describe('Location.targetLocation', () => {
 });
 
 describe('Location.getDisplayName', () => {
+  beforeEach(() => {
+    this.xhr = sinon.useFakeXMLHttpRequest();
+    this.requests = [];
+    this.xhr.onCreate = (xhr) => {
+      this.requests.push(xhr);
+    };
+    sinon.spy(Rollbar, 'error');
+  });
+  afterEach(() => {
+    this.xhr.restore();
+    Rollbar.error.restore();
+  });
   it('it returns a location name', (done) => {
     const promise = Location.getDisplayName(Location.targetLocation);
+    expect(this.requests.length).to.equal(1);
+    this.requests[0].respond(200, {}, JSON.stringify(locationData));
     promise.then((data) => {
       expect(data).to.equal('San Francisco, CA');
+      done();
+    }, (error) => {
+      expect.fail(error);
+      done();
+    });
+  });
+  it('will log an error if the status is not ok', (done) => {
+    const promise = Location.getDisplayName(Location.targetLocation);
+    expect(this.requests.length).to.equal(1);
+    this.requests[0].respond(200, {}, JSON.stringify({status: 'ERROR'}));
+    promise.then((data) => {
+      expect(data).to.equal('');
+      expect(Rollbar.error.calledOnce).to.be.true;
+      expect(Rollbar.error.getCall(0).args[0]).to.equal('Failed to geocode');
+      done();
+    }, (error) => {
+      expect.fail(error);
+      done();
+    });
+  });
+  it('will log an error if the xhr errors out', (done) => {
+    const promise = Location.getDisplayName(Location.targetLocation);
+    expect(this.requests.length).to.equal(1);
+    this.requests[0].error();
+    promise.then((data) => {
+      expect(data).to.equal('');
+      expect(Rollbar.error.calledOnce).to.be.true;
+      expect(Rollbar.error.getCall(0).args[0]).to.equal('Failed to geocode');
       done();
     }, (error) => {
       expect.fail(error);
