3562cc4ddb1aac96b88a354904ecce2392f04a5e
Albert Wang <git@albertyw.com>
2019-04-13 13:54:42 -0700

    Have browserify write directly to file

--- bin/browserify.js
+++ bin/browserify.js
@@ -1,9 +1,14 @@
+const fs = require('fs');
+
 const browserify = require('browserify');
 require('dotenv').config();
 
-browserify('codemancer/js/index.js', {debug: true})
+const inputFile = 'codemancer/js/index.js';
+const outputFile = 'codemancer/js/codemancer.min.js';
+
+browserify(inputFile, {debug: true})
   .transform('envify')
   .transform('babelify',  {presets: ['@babel/preset-env']})
   .transform('uglifyify', {compress: true, mangle: true, global: true})
   .bundle()
-  .pipe(process.stdout);
+  .pipe(fs.createWriteStream(outputFile));
