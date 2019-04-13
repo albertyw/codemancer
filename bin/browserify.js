f24a91c32dfa69e23321ac34e6a602450a3e3d64
Albert Wang <git@albertyw.com>
2019-04-13 15:21:23 -0700

    Refactor filename generation to util

--- bin/browserify.js
+++ bin/browserify.js
@@ -1,20 +1,12 @@
-const child_process = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
 const browserify = require('browserify');
 require('dotenv').config();
-const util = require('../codemancer/js/util');
-
-function getOutputFileName() {
-  const head = child_process.execSync('git rev-parse HEAD');
-  const version = util.trimString(head.toString());
-  const outputFileName = 'codemancer.' + version + '.min.js';
-  return outputFileName;
-}
+const util = require('../server/util');
 
 const inputFile = path.join(__dirname, '..', 'codemancer', 'js', 'index.js');
-const outputFile = path.join(__dirname, '..', 'codemancer', 'js', getOutputFileName());
+const outputFile = path.join(__dirname, '..', 'codemancer', 'js', util.getJSFileName());
 
 browserify(inputFile, {debug: true})
   .transform('envify')
