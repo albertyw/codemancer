0f4063d1acd62e4e6f7e81ef92cd5e73721ee8ee
Albert Wang <git@albertyw.com>
2019-04-13 15:09:43 -0700

    Randomize minified javascript file names

--- bin/browserify.js
+++ bin/browserify.js
@@ -1,11 +1,20 @@
+const child_process = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
 const browserify = require('browserify');
 require('dotenv').config();
+const util = require('../codemancer/js/util');
+
+function getOutputFileName() {
+  const head = child_process.execSync('git rev-parse HEAD');
+  const version = util.trimString(head.toString());
+  const outputFileName = 'codemancer.' + version + '.min.js';
+  return outputFileName;
+}
 
 const inputFile = path.join(__dirname, '..', 'codemancer', 'js', 'index.js');
-const outputFile = path.join(__dirname, '..', 'codemancer', 'js', 'codemancer.min.js');
+const outputFile = path.join(__dirname, '..', 'codemancer', 'js', getOutputFileName());
 
 browserify(inputFile, {debug: true})
   .transform('envify')
