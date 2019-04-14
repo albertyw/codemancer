0ca0a67f5c6dc0d66c7dfb34ad63152af4225908
Albert Wang <git@albertyw.com>
2019-04-13 20:23:28 -0700

    Update templateVars to be a shared local variable

--- server/app.js
+++ server/app.js
@@ -42,18 +42,19 @@ app.set('views', path.join(__dirname, '..', 'codemancer'));
 app.set('view engine','html');
 
 app.locals.svg = util.getSVGs();
+app.locals.templateVars = {
+  SEGMENT_TOKEN: process.env.SEGMENT_TOKEN,
+  LOGFIT_TOKEN: process.env.LOGFIT_TOKEN,
+  SUNRISESUNSET_SVG: app.locals.svg.sunrisesunset,
+  TOGGLEDEMO_SVG: app.locals.svg.toggledemo,
+  CALENDAR_AUTH_SVG: app.locals.svg.calendarAuth,
+  CALENDAR_SIGNOUT_SVG: app.locals.svg.calendarSignout,
+  JAVASCRIPT: util.getJSFileName(),
+};
+
 
 app.get('/', (req, res) => {
-  const templateVars = {
-    SEGMENT_TOKEN: process.env.SEGMENT_TOKEN,
-    LOGFIT_TOKEN: process.env.LOGFIT_TOKEN,
-    SUNRISESUNSET_SVG: app.locals.svg.sunrisesunset,
-    TOGGLEDEMO_SVG: app.locals.svg.toggledemo,
-    CALENDAR_AUTH_SVG: app.locals.svg.calendarAuth,
-    CALENDAR_SIGNOUT_SVG: app.locals.svg.calendarSignout,
-    JAVASCRIPT: util.getJSFileName(),
-  };
-  res.render('index', templateVars);
+  res.render('index', app.locals.templateVars);
 });
 app.use('/css', express.static(path.join(__dirname, '..', 'codemancer', 'css')));
 app.use('/font', express.static(path.join(__dirname, '..', 'codemancer', 'font')));
