97c0268f3f4eb847a1af36fe3907991ae1727fb0
Albert Wang <git@albertyw.com>
2019-05-04 16:58:01 -0700

    Add codemancer docker network

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,6 +13,8 @@ git pull
 
 # Build and start container
 docker build -t codemancer:production .
+docker network inspect "codemancer" &>/dev/null ||
+    docker network create --driver bridge "codemancer"
 docker stop codemancer || echo
 docker container prune --force --filter "until=336h"
 docker container rm codemancer
@@ -20,6 +22,7 @@ docker run \
     --detach \
     --restart always \
     --publish 127.0.0.1:5002:5002 \
+    --network="codemancer" \
     --mount type=bind,source="$(pwd)"/logs,target=/var/www/app/logs \
     --name codemancer codemancer:production
 
