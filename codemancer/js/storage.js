8d3347ab8ff108b2d9d7f1123b2995458f18b794
Albert Wang <git@albertyw.com>
2019-12-01 00:20:13 -0800

    Refactor storage

--- codemancer/js/storage.js
+++ codemancer/js/storage.js
@@ -1,24 +1,33 @@
 const localStorage = window.localStorage;
 
 const weatherKey = 'weather';
-const weatherTimeKey = 'weatherTime';
 const weatherExpiration = 3 * 60 * 60 * 1000; // 3 hours
 
 const Storage = {
   setWeatherData: function setWeatherData(data) {
-    localStorage.setItem(weatherKey, data);
-    const timestamp = Date.now().toString();
-    localStorage.setItem(weatherTimeKey, timestamp);
+    return Storage.setExpirableData(weatherKey, data);
   },
 
   getWeatherData: function getWeatherData() {
-    const timestampString = localStorage.getItem(weatherTimeKey);
+    return Storage.getExpirableData(weatherKey, weatherExpiration);
+  },
+
+  setExpirableData: function setExpirableData(key, value) {
+    localStorage.setItem(key, value);
+    const expireKey = key + 'Time';
+    const timestamp = Date.now().toString();
+    localStorage.setItem(expireKey, timestamp);
+  },
+
+  getExpirableData: function getExpirableData(key, expirationDuration) {
+    const expireKey = key + 'Time';
+    const timestampString = localStorage.getItem(expireKey);
     const timestamp = parseInt(timestampString, 10);
-    if (timestamp + weatherExpiration < Date.now()) {
-      localStorage.removeItem(weatherKey);
-      localStorage.removeItem(weatherTimeKey);
+    if (timestamp + expirationDuration < Date.now()) {
+      localStorage.removeItem(key);
+      localStorage.removeItem(expireKey);
     }
-    const data = localStorage.getItem(weatherKey);
+    const data = localStorage.getItem(key);
     return data;
   },
 };
