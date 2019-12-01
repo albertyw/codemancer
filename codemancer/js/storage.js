95ff38a5c46ec3cdef3d682c839fa192910e162a
Albert Wang <git@albertyw.com>
2019-11-30 23:31:21 -0800

    Add expiration of weather data

--- codemancer/js/storage.js
+++ codemancer/js/storage.js
@@ -1,13 +1,23 @@
 const localStorage = window.localStorage;
 
 const weatherKey = 'weather';
+const weatherTimeKey = 'weatherTime';
+const weatherExpiration = 3 * 60 * 60 * 1000; // 3 hours
 
 const Storage = {
   setWeatherData: function setWeatherData(data) {
     localStorage.setItem(weatherKey, data);
+    const timestamp = Date.now().toString();
+    localStorage.setItem(weatherTimeKey, timestamp);
   },
 
   getWeatherData: function getWeatherData() {
+    const timestampString = localStorage.getItem(weatherTimeKey);
+    const timestamp = parseInt(timestampString, 10);
+    if (timestamp + weatherExpiration < Date.now()) {
+      localStorage.removeItem(weatherKey);
+      localStorage.removeItem(weatherTimeKey);
+    }
     const data = localStorage.getItem(weatherKey);
     return data;
   },
