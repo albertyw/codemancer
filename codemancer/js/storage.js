7ed1d60d4c45add31d02b747d2f22abfdaf7340c
Albert Wang <git@albertyw.com>
2019-12-01 00:24:44 -0800

    Refactor

--- codemancer/js/storage.js
+++ codemancer/js/storage.js
@@ -14,13 +14,13 @@ const Storage = {
 
   setExpirableData: function setExpirableData(key, value) {
     localStorage.setItem(key, value);
-    const expireKey = key + 'Time';
+    const expireKey = Storage.expireKey(key);
     const timestamp = Date.now().toString();
     localStorage.setItem(expireKey, timestamp);
   },
 
   getExpirableData: function getExpirableData(key, expirationDuration) {
-    const expireKey = key + 'Time';
+    const expireKey = Storage.expireKey(key);
     const timestampString = localStorage.getItem(expireKey);
     const timestamp = parseInt(timestampString, 10);
     if (timestamp + expirationDuration < Date.now()) {
@@ -29,6 +29,10 @@ const Storage = {
     }
     const data = localStorage.getItem(key);
     return data;
+  },
+
+  expireKey: function expireKey(key) {
+    return key + 'Time';
   },
 };
 
