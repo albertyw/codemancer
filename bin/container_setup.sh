17fb8b843426b19d317b4ee9f72cf148b44aceee
Albert Wang <git@albertyw.com>
2019-12-29 15:02:10 +0800

    Update Dockerfile and container setup

--- bin/container_setup.sh
+++ bin/container_setup.sh
@@ -6,22 +6,6 @@ IFS=$'\n\t'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$DIR/.." || exit 1
 
-# Set locale
-sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && locale-gen
-
-# Install system dependencies
-apt-get update
-apt-get install -y build-essential curl locales software-properties-common
-apt-get install -y gcc g++ make
-
-# Install node dependencies
-curl -sL https://deb.nodesource.com/setup_11.x | bash -
-apt-get update
-apt-get install -y git nodejs
-
-# Set up .env
-ln -s .env.production .env
-
 # Compile code
 npm install
 npm run minify
