019809bef3fc45243fca6928a8d1a847f578d01a
Albert Wang <git@albertyw.com>
2019-12-29 15:03:39 +0800

    Rename environment to env

--- codemancer/js/varsnap.js
+++ codemancer/js/varsnap.js
@@ -2,7 +2,7 @@ const process = require('process');
 const varsnap = require('varsnap');
 varsnap.config = {
   varsnap: process.env.VARSNAP,
-  env: process.env.ENVIRONMENT,
+  env: process.env.ENV,
   producerToken: process.env.VARSNAP_PRODUCER_TOKEN,
   consumerToken: process.env.VARSNAP_CONSUMER_TOKEN,
 };
