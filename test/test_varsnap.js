740536e5af4fa597b69234841f8ca2c34ef69d30
Albert Wang <git@albertyw.com>
2020-04-01 02:21:01 -0700

    Update varsnap to 1.3.0

--- test/test_varsnap.js
+++ test/test_varsnap.js
@@ -1,6 +1,13 @@
+const expect = require('chai').expect;
 const varsnap = require('varsnap');
 
 require('../codemancer/js/background');
 // require('../server/util');
 
-varsnap.TestCase();
+context('Varsnap', function() {
+  this.timeout(30 * 1000);
+  it('runs with production', async function() {
+    const status = await varsnap.runTests();
+    expect(status).to.be.true;
+  });
+});
