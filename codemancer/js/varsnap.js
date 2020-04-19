b8792893929df4e4ca2ada0948000e043df70758
Albert Wang <git@albertyw.com>
2020-04-19 15:20:09 -0700

    Npm update

--- codemancer/js/varsnap.js
+++ codemancer/js/varsnap.js
@@ -1,10 +1,10 @@
 const process = require('process');
 const varsnap = require('varsnap');
-varsnap.config = {
+varsnap.updateConfig({
   varsnap: process.env.VARSNAP,
   env: process.env.ENV,
   producerToken: process.env.VARSNAP_PRODUCER_TOKEN,
   consumerToken: process.env.VARSNAP_CONSUMER_TOKEN,
-};
+});
 
 module.exports = varsnap;
