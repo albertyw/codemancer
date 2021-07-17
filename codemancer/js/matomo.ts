f631d93aa08f0bfb140126f119a7506c888b6dcf
Albert Wang <git@albertyw.com>
2021-07-17 01:26:48 -0700

    Fix lint issues

--- codemancer/js/matomo.ts
+++ codemancer/js/matomo.ts
@@ -1,11 +1,13 @@
-var _paq = window._paq = window._paq || [];
+/* eslint-disable  @typescript-eslint/ban-ts-comment */
+// @ts-nocheck Copied from matomo
+const _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="//matomo.albertyw.com/";
+  const u='//matomo.albertyw.com/';
   _paq.push(['setTrackerUrl', u+'matomo.php']);
   _paq.push(['setSiteId', '2']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  const d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
   g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
