e1f05e6c5204906aa2e3284964a1484a15854631
Albert Wang <git@albertyw.com>
2022-02-27 19:19:22 -0500

    Simplify docker commands to also pull base images

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -25,8 +25,7 @@ if [ -n "$DEPLOY_BRANCH" ]; then
 fi
 
 # Build container and network
-docker pull "$(grep FROM Dockerfile | awk '{print $2}')"
-docker build -t "$CONTAINER:$BRANCH" .
+docker build --pull -t "$CONTAINER:$BRANCH" .
 docker network inspect "$NETWORK" &>/dev/null ||
     docker network create --driver bridge "$NETWORK"
 
