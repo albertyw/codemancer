efe8e9c8d4aa5cc251afbe3c44e0a4288bda2c6a
Albert Wang <git@albertyw.com>
2022-07-11 00:39:10 -0700

    Fix linting

--- codemancer/js/ganalytics.ts
+++ codemancer/js/ganalytics.ts
@@ -1,9 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 function setupGoogleAnalytics() {
-  var script = document.createElement('script');
+  const script = document.createElement('script');
   script.onload = function () {
     window.dataLayer = window.dataLayer || [];
-    function gtag(){window.dataLayer.push(arguments);}
+    function gtag(...args){window.dataLayer.push(args);}
     gtag('js', new Date());
     gtag('config', process.env.GOOGLE_ANALYTICS_TOKEN);
   };
