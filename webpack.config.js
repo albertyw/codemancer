f31286c16804e44bc00328ee6d641c921b143595
Albert Wang <git@albertyw.com>
2023-04-18 22:58:22 -0700

    Enable sourcemaps for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ const config = {
     path: path.resolve(__dirname, "codemancer", "js"),
     filename: "codemancer.min.js",
   },
+  devtool: 'inline-source-map',
   plugins: [
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
