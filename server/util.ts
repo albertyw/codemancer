f3691255538860e19f6b402ae86ca5062382c028
Albert Wang <git@albertyw.com>
2024-12-26 02:45:46 -0800

    Fix types for server/util.ts

--- server/util.ts
+++ server/util.ts
@@ -8,9 +8,9 @@ import varsnap from '../codemancer/js/varsnap.js';
 const appRoot = appRootPath.toString();
 
 export const getSVGs: () => Promise<{ [key: string]: string }> = varsnap(function getSVGs(): Promise<{ [key: string]: string }> {
-  const svgs = {};
-  function readSVGFile(svgFile, svgName) {
-    return (resolve, reject) => {
+  const svgs: { [key: string]: string } = {};
+  function readSVGFile(svgFile: string, svgName: string) {
+    return new Promise<string>((resolve, reject) => {
       const svgPath = path.join(appRoot, 'codemancer', 'img', svgFile);
       fs.readFile(svgPath, (err, data) => {
         if (err) {
@@ -19,11 +19,11 @@ export const getSVGs: () => Promise<{ [key: string]: string }> = varsnap(functio
         svgs[svgName] = data.toString();
         return resolve(data.toString());
       });
-    };
+    });
   }
   const readers = [
-    new Promise(readSVGFile('github.svg', 'github')),
-    new Promise(readSVGFile('toggledemo.svg', 'toggledemo')),
+    readSVGFile('github.svg', 'github'),
+    readSVGFile('toggledemo.svg', 'toggledemo'),
   ];
   return Promise.all(readers).then(() => {
     return svgs;
