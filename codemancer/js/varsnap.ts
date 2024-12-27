4c252c7309d20ce97f079d2c1a1d5c2ee42a4f34
Albert Wang <git@albertyw.com>
2024-12-27 14:54:55 -0800

    Fix types for codemancer/js/varsnap.ts

--- codemancer/js/varsnap.ts
+++ codemancer/js/varsnap.ts
@@ -2,10 +2,10 @@ import varsnap from 'varsnap';
 
 varsnap.updateConfig({
   varsnap: process.env.VARSNAP === 'true',
-  env: process.env.ENV,
-  branch: process.env.GIT_BRANCH,
-  producerToken: process.env.VARSNAP_PRODUCER_TOKEN,
-  consumerToken: process.env.VARSNAP_CONSUMER_TOKEN,
+  env: process.env.ENV || '',
+  branch: process.env.GIT_BRANCH || '',
+  producerToken: process.env.VARSNAP_PRODUCER_TOKEN || '',
+  consumerToken: process.env.VARSNAP_CONSUMER_TOKEN || '',
 });
 
 export default varsnap;
