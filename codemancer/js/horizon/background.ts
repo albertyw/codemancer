8f41202a6e14573e5bf9f65f5fc4f632465e1e35
Albert Wang <git@albertyw.com>
2025-11-26 22:07:24 -0800

    Refactor horizon/background into a class

--- codemancer/js/horizon/background.ts
+++ codemancer/js/horizon/background.ts
@@ -2,11 +2,13 @@ import suncalc from 'suncalc';
 
 import renderGradient from './gradient.js';
 import { location } from '../location.js';
-import { LocationData } from '../../../server/location.js';
+import { targetLocation, LocationData } from '../../../server/location.js';
 
-export function load() {
-  location.getLocation().then((locationData: LocationData) => {
-    const sunPos = suncalc.getPosition(new Date(), locationData.lat, locationData.lng);
+export class BackgroundColor {
+  locationData: LocationData = targetLocation;
+
+  update(): void {
+    const sunPos = suncalc.getPosition(new Date(), this.locationData.lat, this.locationData.lng);
 
     const [gradient, topVec, bottomVec] = renderGradient(sunPos.altitude);
 
@@ -15,5 +17,13 @@ export function load() {
     console.log('Background gradient:', { gradient, top, bottom });
 
     document.getElementsByTagName('body')[0].style.background = gradient;
+  }
+}
+
+export function load() {
+  location.getLocation().then((locationData: LocationData) => {
+    const backgroundColor = new BackgroundColor();
+    backgroundColor['locationData'] = locationData;
+    backgroundColor.update();
   });
 }
