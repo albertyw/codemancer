d7766e2fa2ba4279a3b1bb58a8f2b88c70877e94
Albert Wang <git@albertyw.com>
2025-11-26 21:46:36 -0800

    Use current location for horizon background

--- codemancer/js/horizon/background.ts
+++ codemancer/js/horizon/background.ts
@@ -1,22 +1,19 @@
-import renderGradient from './gradient.js';
 import suncalc from 'suncalc';
 
+import renderGradient from './gradient.js';
+import { location } from '../location.js';
+import { LocationData } from '../../../server/location.js';
+
 export function load() {
-  const latitude = '0';
-  const longitude = '0';
+  location.getLocation().then((locationData: LocationData) => {
+    const sunPos = suncalc.getPosition(new Date(), locationData.lat, locationData.lng);
 
-  const now = new Date();
-  const sunPos = suncalc.getPosition(
-    now,
-    parseFloat(latitude),
-    parseFloat(longitude),
-  );
+    const [gradient, topVec, bottomVec] = renderGradient(sunPos.altitude);
 
-  const [gradient, topVec, bottomVec] = renderGradient(sunPos.altitude);
+    const top = `rgb(${topVec[0]}, ${topVec[1]}, ${topVec[2]})`;
+    const bottom = `rgb(${bottomVec[0]}, ${bottomVec[1]}, ${bottomVec[2]})`;
+    console.log('Background gradient:', { gradient, top, bottom });
 
-  const top = `rgb(${topVec[0]}, ${topVec[1]}, ${topVec[2]})`;
-  const bottom = `rgb(${bottomVec[0]}, ${bottomVec[1]}, ${bottomVec[2]})`;
-  console.log('Background gradient:', { gradient, top, bottom });
-
-  document.getElementsByTagName('body')[0].style.background = gradient;
+    document.getElementsByTagName('body')[0].style.background = gradient;
+  });
 }
