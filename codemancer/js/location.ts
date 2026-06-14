a2d656da6bb76f8aa0c395e52302a41fcff5bc8f
Albert Wang <git@albertyw.com>
2026-06-13 17:46:23 -0700

    Have frontend pass location coordinates to backend

--- codemancer/js/location.ts
+++ codemancer/js/location.ts
@@ -4,6 +4,7 @@ import getRollbar from './rollbar.js';
 import { requestPromise } from './util.js';
 import { LocationData } from '../../server/location.js';
 
+const baseURL = '/location/';
 const cacheDuration = 24 * 60 * 60 * 1000;
 const backupDuration = 7 * 24 * 60 * 60 * 1000;
 
@@ -11,21 +12,32 @@ export class Location {
   #locationData: Promise<LocationData>|undefined = undefined;
 
   getLocation(): Promise<LocationData> {
-    const url = '/location/';
     if (this.#locationData !== undefined) {
       return this.#locationData;
     }
-    this.#locationData = <Promise<LocationData>>requestPromise(url, cacheDuration, backupDuration)
-      .then((data) => {
-        return data;
+    this.#locationData = new Promise<GeolocationCoordinates>((resolve, reject) => {
+      return navigator.geolocation.getCurrentPosition((position) => {
+        return resolve(position.coords);
       }, (error) => {
-        getRollbar().error('Failed to geocode', error);
-        return {};
-      })
-      .catch((error) => {
-        getRollbar().error('Failed to geocode', error);
-        return {};
+        getRollbar().error('Failed to get geolocation', error);
+        reject(error);
       });
+    }).then((coordinates: GeolocationCoordinates) => {
+      const params = new URLSearchParams({
+        latitude: coordinates.latitude.toString(),
+        longitude: coordinates.longitude.toString(),
+      });
+      const url = `${baseURL}?${params.toString()}`;
+      return requestPromise(url, cacheDuration, backupDuration);
+    }).then((data) => {
+      return data;
+    }, (error) => {
+      getRollbar().error('Failed to geocode', error);
+      return {};
+    }).catch((error) => {
+      getRollbar().error('Failed to geocode', error);
+      return {};
+    }) as Promise<LocationData>;
     return this.#locationData;
   }
 
